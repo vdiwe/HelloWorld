fdba9a5d985615af541a7e4df2832e8e06876d57
Vachaspati <vachhy@gmail.com>
2016-08-11 12:16:39 -0400

    commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String [] args){
 		System.out.print("Hello World");
 		System.out.print("Add a+b");
+		System.out.print("Add a+b+c");
 
 		
 	}
--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -4,6 +4,7 @@ public class HelloWorld2 {
 	public void Helloworld2(){
 		System.out.print("I have added this new code");
 		System.out.print("I have added this new code2");
+		System.out.print("I have added this new code3");
 	}
 
 }
